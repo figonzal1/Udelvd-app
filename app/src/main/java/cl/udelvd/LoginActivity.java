5733fccdad1e4f2da196d2b222eb5ecdd7dc3798
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2019-11-18 16:30:04 -0300

    Aplicacion realiza login correcto, con obtencion de token JWT de servidor
    Signed-off-by: figonzal1 <felipe.gonzalezalarcon94@gmail.com>

--- app/src/main/java/cl/udelvd/LoginActivity.java
+++ app/src/main/java/cl/udelvd/LoginActivity.java
@@ -6,18 +6,27 @@ import android.text.Spannable;
 import android.text.TextUtils;
 import android.text.method.LinkMovementMethod;
 import android.text.style.ClickableSpan;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
+import java.util.Objects;
+
+import cl.udelvd.model.Investigador;
+import cl.udelvd.repositorios.InvestigadorRepositorio;
 import cl.udelvd.utils.Utils;
+import cl.udelvd.viewmodel.InvestigadorViewModel;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -31,11 +40,42 @@ public class LoginActivity extends AppCompatActivity {
 
     private TextView tv_registro;
 
+    private ProgressBar progressBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        InvestigadorViewModel investigadorViewModel =
+                ViewModelProviders.of(this).get(InvestigadorViewModel.class);
+
+        investigadorViewModel.mostrarMsgRespuesta().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+
+                progressBar.setVisibility(View.INVISIBLE);
+                Log.d("VIEW_MODEL", "MSG_RESPONSE: " + s);
+
+                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+
+                if (s.equals("¡Bienvenido!")) {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+
+            }
+        });
+
+        investigadorViewModel.mostrarErrorRespuesta().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                progressBar.setVisibility(View.INVISIBLE);
+                Log.d("VIEW_MODEL", "MSG_ERROR: " + s);
+                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+            }
+        });
 
         //Instancia formulario
         //Inputs Layouts
@@ -46,13 +86,24 @@ public class LoginActivity extends AppCompatActivity {
         etEmail = findViewById(R.id.et_email_login);
         etPassword = findViewById(R.id.et_password_login);
 
+        progressBar = findViewById(R.id.progress_horizontal_login);
+
         btn_login = findViewById(R.id.btn_login);
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 if (validarCampos()) {
-                    Toast.makeText(getApplicationContext(), "Login Valido", Toast.LENGTH_LONG).show();
+                    progressBar.setVisibility(View.VISIBLE);
+
+                    Investigador investigador = new Investigador();
+                    investigador.setEmail(Objects.requireNonNull(etEmail.getText()).toString().toLowerCase());
+                    investigador.setPassword(Objects.requireNonNull(etPassword.getText()).toString());
+
+                    InvestigadorRepositorio investigadorRepositorio =
+                            InvestigadorRepositorio.getInstance(getApplication());
+
+                    investigadorRepositorio.loginInvestigador(investigador);
                 }
             }
         });

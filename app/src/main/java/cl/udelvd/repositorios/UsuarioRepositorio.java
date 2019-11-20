671f31286389b5349991befcaffad611f962f7c3
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2019-11-20 20:56:14 -0300

    Avances en la verificación del token de login y cambio de actividad launcher
    Signed-off-by: figonzal1 <felipe.gonzalezalarcon94@gmail.com>

---
+++ app/src/main/java/cl/udelvd/repositorios/UsuarioRepositorio.java
@@ -0,0 +1,117 @@
+package cl.udelvd.repositorios;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+import androidx.lifecycle.MutableLiveData;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import cl.udelvd.model.Usuario;
+import cl.udelvd.services.VolleySingleton;
+import cl.udelvd.utils.SingleLiveEvent;
+
+public class UsuarioRepositorio {
+
+    private static UsuarioRepositorio instancia;
+    private Application application;
+    private MutableLiveData<List<Usuario>> mutableUsuariosList = new MutableLiveData<>();
+    private SingleLiveEvent<Boolean> validToken = new SingleLiveEvent<>();
+    private List<Usuario> usuarioList;
+
+    private UsuarioRepositorio(Application application) {
+        this.application = application;
+    }
+
+    public static UsuarioRepositorio getInstance(Application application) {
+
+        if (instancia == null) {
+            instancia = new UsuarioRepositorio(application);
+        }
+        return instancia;
+    }
+
+    //TODO: Terminar obtencion de listado de usuarios
+    public MutableLiveData<List<Usuario>> getUsuarios() {
+        sendGetUsuarios();
+        return null;
+    }
+
+    public SingleLiveEvent<Boolean> isValidToken() {
+        return validToken;
+    }
+
+    private void sendGetUsuarios() {
+
+        final Response.Listener<String> responseListener = new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d("RESPONSE", response);
+                validToken.postValue(true);
+            }
+        };
+
+        final Response.ErrorListener errorListener = new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                if (error.networkResponse != null && error.networkResponse.data != null) {
+
+                    String json = new String(error.networkResponse.data);
+
+                    Log.d("Json", json);
+
+                    try {
+                        JSONObject jsonObject = new JSONObject(json);
+
+                        JSONObject jsonError = jsonObject.getJSONObject("error");
+
+                        String detail_error = jsonError.getString("detail");
+
+                        if (detail_error.equals("Token invalid")) {
+                            validToken.postValue(false);
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+
+        String url = "http://192.168.0.14/usuarios";
+
+        //Hacer request
+        StringRequest request = new StringRequest(Request.Method.GET, url, responseListener,
+                errorListener) {
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                SharedPreferences sharedPreferences = application.getSharedPreferences("udelvd",
+                        Context.MODE_PRIVATE);
+
+                String token = sharedPreferences.getString("TOKEN_LOGIN", "");
+
+                Map<String, String> params = new HashMap<>();
+                params.put("Authorization", "Bearer " + token);
+                return params;
+            }
+        };
+
+        String TAG = "ListaUsuario";
+        VolleySingleton.getInstance(application).addToRequestQueue(request, TAG);
+    }
+
+}

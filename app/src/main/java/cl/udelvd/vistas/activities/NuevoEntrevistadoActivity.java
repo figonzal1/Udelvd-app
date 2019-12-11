26a82f2300e9140b31370ff3e79ab84e36fbbf2f
Felipe Gonzalez Alarcon <Felipe.gonzalezalarcon94@gmail.com>
2019-12-11 16:45:25 -0300

    Avances en formulario de creacion de persona, tentativo de autocompletacion de ciudades
    Signed-off-by: Felipe Gonzalez Alarcon <Felipe.gonzalezalarcon94@gmail.com>

---
+++ app/src/main/java/cl/udelvd/vistas/activities/NuevoEntrevistadoActivity.java
@@ -0,0 +1,130 @@
+package cl.udelvd.vistas.activities;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatAutoCompleteTextView;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.Calendar;
+import java.util.Objects;
+
+import cl.udelvd.R;
+
+public class NuevoEntrevistadoActivity extends AppCompatActivity {
+
+    private TextInputLayout ilNombre;
+    private TextInputLayout ilApellido;
+    private TextInputLayout ilFechaNacimiento;
+    private TextInputLayout ilCiudad;
+
+    private TextInputEditText etNombre;
+    private TextInputEditText etApellido;
+    private TextInputEditText etFechaNacimiento;
+    private AppCompatAutoCompleteTextView acCiudad;
+
+    private MaterialButton btn_fecha_nacimiento;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nuevo_usuario);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitleTextColor(getResources().getColor(R.color.colorOnPrimary));
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_close_white_24dp);
+        actionBar.setTitle("Crear entrevistado");
+
+
+        ilNombre = findViewById(R.id.il_nombre_entrevistado);
+        ilApellido = findViewById(R.id.il_apellido_entrevistado);
+        ilFechaNacimiento = findViewById(R.id.il_fecha_nacimiento);
+        ilCiudad = findViewById(R.id.il_ciudad_entrevistado);
+
+        etNombre = findViewById(R.id.et_nombre_entrevistado);
+        etApellido = findViewById(R.id.et_apellido_entrevistado);
+        etFechaNacimiento = findViewById(R.id.et_fecha_nacimiento);
+        acCiudad = findViewById(R.id.et_ciudad_entrevistado);
+
+        String[] countries = new String[]{
+                "La serena", "Valparaiso", "Viña del mar", "Concepción", "Santiago", "La florida", "La Pintana", "Lo barnechea", "Lo Espejo",
+                "La ligua", "La Serena", "LS"
+        };
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, countries);
+        acCiudad.setAdapter(adapter);
+
+
+        ilFechaNacimiento.setEndIconOnClickListener(new View.OnClickListener() {
+
+            int year;
+            int month;
+            int day;
+
+            @Override
+            public void onClick(View v) {
+
+                final Calendar c = Calendar.getInstance();
+                year = c.get(Calendar.YEAR);
+                month = c.get(Calendar.MONTH);
+                day = c.get(Calendar.DAY_OF_MONTH);
+
+
+                if (Objects.requireNonNull(etFechaNacimiento.getText()).length() > 0) {
+
+                    String fecha = etFechaNacimiento.getText().toString();
+                    String[] fecha_split = fecha.split("-");
+
+                    year = Integer.parseInt(fecha_split[0]);
+                    month = Integer.parseInt(fecha_split[1]);
+                    day = Integer.parseInt(fecha_split[2]);
+                }
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(NuevoEntrevistadoActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        etFechaNacimiento.setText(year + "-" + month + "-" + dayOfMonth);
+                    }
+                }, year, month, day);
+
+                datePickerDialog.show();
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_guardar_datos, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}

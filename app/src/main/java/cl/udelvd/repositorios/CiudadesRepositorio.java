91806e07c8079cc645153b95892394d895cb9b04
Felipe Gonzalez Alarcon <Felipe.gonzalezalarcon94@gmail.com>
2019-12-12 00:44:12 -0300

    Autocompletado ciudad carga ciudades desde BD
    Signed-off-by: Felipe Gonzalez Alarcon <Felipe.gonzalezalarcon94@gmail.com>

---
+++ app/src/main/java/cl/udelvd/repositorios/CiudadesRepositorio.java
@@ -0,0 +1,158 @@
+package cl.udelvd.repositorios;
+
+
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+import androidx.lifecycle.MutableLiveData;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.ServerError;
+import com.android.volley.TimeoutError;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import cl.udelvd.modelo.Ciudad;
+import cl.udelvd.servicios.VolleySingleton;
+
+public class CiudadesRepositorio {
+
+    private static CiudadesRepositorio instancia;
+    private final Application application;
+
+    private List<Ciudad> ciudadList;
+    private MutableLiveData<List<Ciudad>> mutableCiudadList = new MutableLiveData<>();
+
+    private CiudadesRepositorio(Application application) {
+        this.application = application;
+    }
+
+    public static CiudadesRepositorio getInstancia(Application application) {
+        if (instancia == null) {
+            instancia = new CiudadesRepositorio(application);
+        }
+        return instancia;
+    }
+
+
+    public MutableLiveData<List<Ciudad>> obtenerCiudades() {
+        enviarGetCiudades();
+        return mutableCiudadList;
+    }
+
+    private void enviarGetCiudades() {
+
+        ciudadList = new ArrayList<>();
+
+        Response.Listener<String> responseListener = new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d("RESPONSE", response);
+
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+
+                    JSONArray jsonData = jsonObject.getJSONArray("data");
+
+                    for (int i = 0; i < jsonData.length(); i++) {
+                        JSONObject jsonCiudad = jsonData.getJSONObject(i);
+
+                        int id_ciudad = jsonCiudad.getInt("id");
+                        JSONObject jsonAttributes = jsonCiudad.getJSONObject("attributes");
+                        String nombre_ciudad = jsonAttributes.getString("nombre");
+
+                        Ciudad ciudad = new Ciudad();
+                        ciudad.setId(id_ciudad);
+                        ciudad.setNombre(nombre_ciudad);
+
+                        ciudadList.add(ciudad);
+
+                    }
+
+                    mutableCiudadList.postValue(ciudadList);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        Response.ErrorListener errorListener = new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                if (error instanceof TimeoutError) {
+                    Log.d("VOLLEY_ERROR_LOGIN", "TIMEOUT_ERROR");
+                }
+
+                //Error de conexion a internet
+                else if (error instanceof NetworkError) {
+                    Log.d("VOLLEY_ERROR_LOGIN", "NETWORK_ERROR");
+                }
+
+                //Errores cuando el servidor si responde
+                else if (error.networkResponse != null && error.networkResponse.data != null) {
+
+                    String json = new String(error.networkResponse.data);
+
+                    JSONObject errorObject = null;
+
+                    //Obtener json error
+                    try {
+                        JSONObject jsonObject = new JSONObject(json);
+                        errorObject = jsonObject.getJSONObject("error");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                    //Error de autorizacion
+                    if (error instanceof AuthFailureError) {
+                        Log.d("VOLLEY_ERROR_LOGIN", "AUTHENTICATION_ERROR: " + errorObject);
+                    }
+
+                    //Error de servidor
+                    else if (error instanceof ServerError) {
+                        Log.d("VOLLEY_ERROR_LOGIN", "SERVER_ERROR: " + errorObject);
+                    }
+                }
+            }
+        };
+
+
+        String url = "http://192.168.0.14/ciudades";
+
+        StringRequest request = new StringRequest(Request.Method.GET, url, responseListener, errorListener) {
+
+            @Override
+            public Map<String, String> getHeaders() {
+
+                SharedPreferences sharedPreferences = application.getSharedPreferences("udelvd",
+                        Context.MODE_PRIVATE);
+
+                String token = sharedPreferences.getString("TOKEN_LOGIN", "");
+
+                Map<String, String> params = new HashMap<>();
+                params.put("Content-Type", "application/x-www-form-urlencoded");
+                params.put("Authorization", "Bearer " + token);
+                return params;
+            }
+        };
+
+        String TAG = "ciudades";
+        VolleySingleton.getInstance(application).addToRequestQueue(request, TAG);
+
+    }
+}

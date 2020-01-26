130805b0d70624c2529eebeae15387616032ea7a
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2020-01-26 17:54:00 -0300

    Implementacion de entrevistados viewmodel
    Signed-off-by: figonzal1 <felipe.gonzalezalarcon94@gmail.com>

---
+++ app/src/main/java/cl/udelvd/viewmodel/EntrevistadoListaViewModel.java
@@ -0,0 +1,42 @@
+package cl.udelvd.viewmodel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MutableLiveData;
+
+import java.util.List;
+
+import cl.udelvd.modelo.Entrevistado;
+import cl.udelvd.repositorios.EntrevistadoRepositorio;
+import cl.udelvd.utilidades.SingleLiveEvent;
+
+public class EntrevistadoListaViewModel extends AndroidViewModel {
+
+    private EntrevistadoRepositorio entrevistadoRepositorio;
+
+    public EntrevistadoListaViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+    public MutableLiveData<Boolean> isLoading() {
+        entrevistadoRepositorio = EntrevistadoRepositorio.getInstance(getApplication());
+        return entrevistadoRepositorio.getIsLoading();
+    }
+
+    public MutableLiveData<List<Entrevistado>> cargarListaEntrevistados() {
+        entrevistadoRepositorio = EntrevistadoRepositorio.getInstance(getApplication());
+        return entrevistadoRepositorio.obtenerEntrevistados();
+    }
+
+    public SingleLiveEvent<String> mostrarMsgErrorListado() {
+        entrevistadoRepositorio = EntrevistadoRepositorio.getInstance(getApplication());
+        return entrevistadoRepositorio.getResponseMsgErrorListado();
+    }
+
+    public void refreshListaEntrevistados() {
+        entrevistadoRepositorio = EntrevistadoRepositorio.getInstance(getApplication());
+        entrevistadoRepositorio.obtenerEntrevistados();
+    }
+}

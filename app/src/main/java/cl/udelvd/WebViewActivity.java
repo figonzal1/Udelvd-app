9313096deb0610b723addc69db0fc77a30e89a92
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2020-02-08 17:36:24 -0300

    Avances en estadisticas
    Signed-off-by: figonzal1 <felipe.gonzalezalarcon94@gmail.com>

---
+++ app/src/main/java/cl/udelvd/WebViewActivity.java
@@ -0,0 +1,51 @@
+package cl.udelvd;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+
+        WebView web_view = findViewById(R.id.webview);
+        web_view.setVerticalScrollBarEnabled(true);
+        web_view.requestFocus();
+        web_view.getSettings().setDefaultTextEncodingName("utf-8");
+        web_view.getSettings().setAppCacheEnabled(false);
+        web_view.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+        web_view.getSettings().setJavaScriptEnabled(true);
+        web_view.getSettings().setBuiltInZoomControls(true);
+        //web_view.loadUrl("https://public.tableau.com/views/Libro1_15810427188200/Dashboard1?:display_count=y&publish=yes&:origin=viz_share_link");
+        web_view.loadUrl("http://192.168.0.22/test");
+        web_view.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(android.webkit.WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        /*final ProgressBar progressBar = findViewById(R.id.progressBar);
+
+        web_view.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int progress) {
+                progressBar.setProgress(0);
+                progressBar.setVisibility(View.VISIBLE);
+                WebViewActivity.this.setProgress(progress * 1000);
+
+                progressBar.incrementProgressBy(progress);
+
+                if (progress == 100) {
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+        });*/
+    }
+}

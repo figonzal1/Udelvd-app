a8e57da672d91ec0b0139a0a21c3916c7a3ed569
figonzal1 <Felipe.gonzalezalarcon94@gmail.com>
2020-03-09 22:56:29 -0300

    Mejoras en interfaz de estadisticas y mejoras en webView activity
    Signed-off-by: figonzal1 <Felipe.gonzalezalarcon94@gmail.com>

--- app/src/main/java/cl/udelvd/vistas/activities/WebViewActivity.java
+++ app/src/main/java/cl/udelvd/vistas/activities/WebViewActivity.java
@@ -2,21 +2,33 @@ package cl.udelvd.vistas.activities;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import cl.udelvd.R;
+import cl.udelvd.utilidades.Utils;
 
 public class WebViewActivity extends AppCompatActivity {
+
+    private String linkGrafico;
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
+
+        Utils.configurarToolbar(this, getApplicationContext(), R.drawable.ic_close_white_24dp, getString(R.string.TITULO_TABLEAU));
+
+        if (getIntent().getExtras() != null) {
+            Bundle bundle = getIntent().getExtras();
+            linkGrafico = bundle.getString(getString(R.string.INTENT_LINK_GRAFICO));
+        }
 
         WebView web_view = findViewById(R.id.webview);
         web_view.setVerticalScrollBarEnabled(true);
@@ -26,7 +38,7 @@ public class WebViewActivity extends AppCompatActivity {
         web_view.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
         web_view.getSettings().setJavaScriptEnabled(true);
         web_view.getSettings().setBuiltInZoomControls(true);
-        web_view.loadUrl("https://public.tableau.com/views/Libro1_15810427188200/Dashboard1?:display_count=y&:origin=viz_share_link");
+        web_view.loadUrl(linkGrafico);
         web_view.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(android.webkit.WebView view, String url) {
@@ -50,5 +62,17 @@ public class WebViewActivity extends AppCompatActivity {
                 }
             }
         });*/
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        //Boton para abrir Navigation Drawer
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/cl/udelvd/vistas/fragments/EstadisticasFragment.java
+++ app/src/main/java/cl/udelvd/vistas/fragments/EstadisticasFragment.java
@@ -1,12 +1,21 @@
 package cl.udelvd.vistas.fragments;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import cl.udelvd.R;
@@ -15,17 +24,12 @@ import cl.udelvd.vistas.activities.WebViewActivity;
 
 public class EstadisticasFragment extends Fragment {
 
+    private final String URL_GRAFICO_DISTRIBUCIONES = "https://rb.gy/eknxh6";
+
     public EstadisticasFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @return A new instance of fragment EstadisticasFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static EstadisticasFragment newInstance() {
         return new EstadisticasFragment();
     }
@@ -40,15 +44,53 @@ public class EstadisticasFragment extends Fragment {
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_estadisticas, container, false);
 
-        Button button = v.findViewById(R.id.btn_stats);
+        configurarBoton(v);
+
+        configurarLinkDirecto(v);
+
+        return v;
+    }
+
+    private void configurarLinkDirecto(View v) {
+        //Logica de textview de registro
+        TextView tv_registro = v.findViewById(R.id.tv_link_corto);
+
+        int index = URL_GRAFICO_DISTRIBUCIONES.indexOf("//");
+        String subtring = URL_GRAFICO_DISTRIBUCIONES.substring(index + 2);
+        tv_registro.setText(String.format(getString(R.string.FORMATO_LINK_DIRECTO), subtring));
+
+        tv_registro.setMovementMethod(LinkMovementMethod.getInstance());
+
+        Spannable spans = (Spannable) tv_registro.getText();
+        ClickableSpan clickSpan = new ClickableSpan() {
+
+            @Override
+            public void onClick(@NonNull View widget) {
+
+                //Configurar portapapeles
+                ClipboardManager clipboardManager = (ClipboardManager) requireActivity().getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clipData = ClipData.newPlainText(getString(R.string.INTENT_LINK_GRAFICO), URL_GRAFICO_DISTRIBUCIONES);
+
+                if (clipboardManager != null) {
+                    clipboardManager.setPrimaryClip(clipData);
+                    Toast.makeText(getContext(), R.string.COPIADO_PORTAPAPELES, Toast.LENGTH_LONG).show();
+                }
+
+            }
+        };
+        spans.setSpan(clickSpan, tv_registro.getText().toString().indexOf("rb"), spans.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+    }
+
+    private void configurarBoton(View v) {
+        Button button = v.findViewById(R.id.btn_go);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getActivity(), WebViewActivity.class);
+                intent.putExtra(getString(R.string.INTENT_LINK_GRAFICO), URL_GRAFICO_DISTRIBUCIONES);
                 startActivity(intent);
             }
         });
-        return v;
     }
 
 }
